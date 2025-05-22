325675d5de1b2355c31d9c728e883a5353babcbf
bjoernboeckle <Bjoern@Boeckle-net.de>
2025-05-22 17:36:03 +0200

    updated versions and fixed prebuild

--- mqtt_presence/version.py
+++ mqtt_presence/version.py
@@ -1,4 +1,4 @@
-VERSION = "0.2.5"
+VERSION = "0.2.4a1"
 NAME = "mqtt-presence"
 DESCRIPTION = "Lightweight MQTT-based presence detection with shutdown support"
 REPOSITORY = "https://github.com/bjoernboeckle/mqtt-presence"
