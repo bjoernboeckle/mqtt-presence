55bb8b088f35b418878d61005bba384f8a3d00bf
Björn Böckle <Bjoern@Boeckle-net.de>
2025-06-02 12:10:22 +0200

    Further test

--- make.bat
+++ make.bat
@@ -13,24 +13,42 @@ IF "%1"=="build" (
     call :run_pytest || goto :pytest_failed
 ) ELSE IF "%1"=="build-exe" (
     call :run_build_exe || goto :run_build_exe_failed
-) ELSE (
-    echo "Usage: make.bat [build|clean|pylint]"
+) ELSE IF "%1"=="installer" (
+    call :run_build_installer || goto :run_build_installer_failed
+) ELSE (    
+    echo "Usage: make.bat [build|clean|pylint|pytest|build-exe|installer]"
 )
 
 
-
 goto :eof
+
+
+:run_build_installer
+echo Creating installer file...
+call :run_build_exe || goto :run_build_exe_failed
+iscc.exe ./installer/mqtt-presence-setup.iss
+goto :eof
+
+
+:run_build_installer_failed
+echo ❌ installer couldn't be build. Aborting.
+exit /b 1
 
 
 
 :run_build_exe
 echo Creating exe file...
-poetry run pyinstaller mqtt-presence.spec
+poetry build
+REM poetry run PyInstaller mqtt-presence.spec
+python -m PyInstaller mqtt-presence.spec
 goto :eof
+
 
 :run_build_exe_failed
 echo ❌ exe couldn't be build. Aborting.
 exit /b 1
+
+
 
 
 :run_pylint
@@ -44,6 +62,7 @@ exit /b 1
 
 
 
+
 :run_pytest
 echo Running pytest...
 poetry run pytest tests
@@ -52,5 +71,3 @@ goto :eof
 :pytest_failed
 echo ❌ pytest reported issues. Aborting.
 exit /b 1
-
-	
